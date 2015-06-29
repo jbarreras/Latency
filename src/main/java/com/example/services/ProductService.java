8bf146e8ebd3dec568502fe86aee11b9bf3001ac
Jorge Barrera <jorge.e.barrera@hotmail.com>
2015-06-29 17:10:25 -0500

    Add New Relic

--- src/main/java/com/example/services/ProductService.java
+++ src/main/java/com/example/services/ProductService.java
@@ -1,45 +1,100 @@
 package com.example.services;
 
-import com.example.models.Product;
-
-import java.util.ArrayList;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.json.simple.JSONObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 @Path("/Product")
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductService {
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAll() {
+	private static String URL = "mongodb://experiment_latency:experiment_latency@ds043971.mongolab.com:43971/experiment_latency";
+	private static String DB = "experiment_latency";
+	private static String CLL = "experiment_latency";
 
-		List<Product> Products = new ArrayList<Product>();
+	@GET
+	@Path("/Filter")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getInfoFilter(@QueryParam("Comercio") String comercio,
+			@QueryParam("Categoria") String categoria,
+			@QueryParam("Producto") String producto) {
+		List<DBObject> myDoc = null;
+		try {
+			MongoClient mongo = new MongoClient(new MongoClientURI(URL));
+			DB db = mongo.getDB(DB);
+
+			DBCollection table = db.getCollection(CLL);
+
+			BasicDBObject searchQuery = new BasicDBObject();
+
+			if (comercio != null && !comercio.equals(""))
+				searchQuery.put("Comercio", comercio);
+
+			if (categoria != null && !categoria.equals(""))
+				searchQuery.put("Categoria", categoria);
+
+			if (producto != null && !producto.equals(""))
+				searchQuery.put("Producto", producto);
+
+			myDoc = table.find(searchQuery).toArray();
+
+		} catch (UnknownHostException e) {
+			return Response.status(500)
+					.header("Access-Control-Allow-Origin", "*")
+					.entity(e.getMessage()).build();
+		}
 		return Response.status(200).header("Access-Control-Allow-Origin", "*")
-				.entity(Products).build();
+				.entity(myDoc.toString()).build();
 	}
 
-	@POST
+	@GET
+	@Path("/MapReduce")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response createProduct(Product Product) {
+	public Response getInfoMapReduce(@QueryParam("Comercio") String comercio,
+			@QueryParam("Categoria") String categoria,
+			@QueryParam("Producto") String producto) {
+		List<DBObject> myDoc = null;
+		try {
+			MongoClient mongo = new MongoClient(new MongoClientURI(URL));
+			DB db = mongo.getDB(DB);
 
-		Product c = new Product();
-		JSONObject rta = new JSONObject();
-		c.setName(Product.getName());
-		c.setDescription(Product.getDescription());
+			DBCollection table = db.getCollection(CLL);
 
+			BasicDBObject searchQuery = new BasicDBObject();
+
+			if (comercio != null && !comercio.equals(""))
+				searchQuery.put("Comercio", comercio);
+
+			if (categoria != null && !categoria.equals(""))
+				searchQuery.put("Categoria", categoria);
+
+			if (producto != null && !producto.equals(""))
+				searchQuery.put("Producto", producto);
+
+			myDoc = table.find(searchQuery).toArray();
+
+		} catch (UnknownHostException e) {
+			return Response.status(500)
+					.header("Access-Control-Allow-Origin", "*")
+					.entity(e.getMessage()).build();
+		}
 		return Response.status(200).header("Access-Control-Allow-Origin", "*")
-				.entity(rta.toJSONString()).build();
+				.entity(myDoc.toString()).build();
 	}
 
 	@OPTIONS
@@ -52,5 +107,4 @@ public class ProductService {
 				.header("Access-Control-Allow-Headers",
 						"AUTHORIZATION, content-type, accept").build();
 	}
-
 }
